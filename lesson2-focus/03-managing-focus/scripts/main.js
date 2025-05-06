1397a604dbe46acd86ea74c7fd5807a0fe88532c
Payton Pietron <paytonpietron@gmail.com>
2025-05-06 14:33:08 -0400

    Manage focus to headers on click

--- lesson2-focus/03-managing-focus/scripts/main.js
+++ lesson2-focus/03-managing-focus/scripts/main.js
@@ -1,8 +1,10 @@
-page('/', function() {
+let isFirstPage = true;
+
+page('/', function () {
   page.redirect('/what-is-vegemite');
 });
 
-page('/:slug', function(context) {
+page('/:slug', function (context) {
   // This will match any value after the first / in the url. For example, if
   // the url was /foo, the value of slug would be "foo".
   var slug = context.params.slug;
@@ -14,11 +16,19 @@ page('/:slug', function(context) {
   oldPage.classList.remove('is-active');
 
   // Add is-active class to new menu item and section using the URL slug
-  var newMenuItem = document.querySelector('#menu [data-page='+slug+']');
-  var newPage = document.querySelector('main [data-page='+slug+']');
+  var newMenuItem = document.querySelector('#menu [data-page=' + slug + ']');
+  var newPage = document.querySelector('main [data-page=' + slug + ']');
   newMenuItem.classList.add('is-active');
   newPage.classList.add('is-active');
 
+  if (isFirstPage) {
+    isFirstPage = false;
+
+    return;
+  }
+
+  // Move focus to heading of new page.
+  newPage.querySelector('h2').focus();
 });
 
 page({
