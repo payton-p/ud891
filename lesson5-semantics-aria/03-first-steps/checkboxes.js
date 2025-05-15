8e046e76329c8da1c7a940fa1cd6a76b7bec3f0c
Payton Pietron <paytonpietron@gmail.com>
2025-05-15 11:54:08 -0400

    Change aria-checked to update with the checkbox state

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -20,7 +20,13 @@
     this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
     this.el.addEventListener('click', this.toggle.bind(this));
 
-    // Any other set-up we want to do here?
+    // Initialize role and aria-checked state.
+    this.el.setAttribute('role', 'checkbox');
+    if (this.el.hasAttribute('checked')) {
+      this.el.setAttribute('aria-checked', 'true');
+    } else {
+      this.el.setAttribute('aria-checked', 'false');
+    }
   }
 
   Checkbox.prototype.handleKeyDown = function (e) {
@@ -37,13 +43,13 @@
     if (this.el.hasAttribute('checked')) {
       this.el.removeAttribute('checked');
 
-      // Hmm.
-
+      // Keep checked attribute and aria-checked in sync.
+      this.el.setAttribute('aria-checked', 'false');
     } else {
       this.el.setAttribute('checked', '');
 
-      // Hmmmmm.
-
+      // Keep checked attribute and aria-checked in sync.
+      this.el.setAttribute('aria-checked', 'true');
     }
   };
 
